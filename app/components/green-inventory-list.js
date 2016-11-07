01a9a29b8bf52c4e5d33f1d4e4f025f3d77e1ab0
Josh Huffman and Matthew Brandenburg <matthew@>
2016-11-07 14:10:27 -0800

    change admin route model to rsvphash

--- app/components/green-inventory-list.js
+++ app/components/green-inventory-list.js
@@ -1,12 +1,5 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  store: Ember.inject.service(),
-  greens: [],
-  init() {
-    let _this = this;
-    this._super(this.get('store').findAll('green').then(function(response) {
-      _this.set('greens', response);
-    }));
-  },
+  
 });
