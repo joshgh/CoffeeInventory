dcaa053912e7e0260bae9d2465e5176ea8e0c92c
Chance Neff and Tim Bourgault <chance@>
2016-11-07 15:53:48 -0800

    further work on order form

--- app/components/order-form.js
+++ app/components/order-form.js
@@ -2,14 +2,19 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   blend: null,
-  model(){
-    console.log(this.get('store'));
-    return this.get('store').query('blends');
-  },
 
   actions: {
-    selectBlend(blend) {
-      this.set('blend', blend);
-    }
+    selectBlend() {
+      this.set('blend', event.target.value);
+      console.log(this.get('blend'));
+    },
+
+    placeOrder() {
+      var params = {
+        name: this.get('blendChoice.name'),
+        quantity: this.get('quantity')
+      };
+      this.sendAction('placeOrder', params);
+    },
   }
 });
