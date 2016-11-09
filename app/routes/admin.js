6ceafc5bf04c0b4c384c803d6d44957e5455592d
Chance Neff and Matt Brandenburg <matt@>
2016-11-09 10:31:40 -0800

    refactor roast checker

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-
+  roastedOrderArray: [],
   model(){
     return Ember.RSVP.hash({
       greens: this.store.findAll('green'),
@@ -27,23 +27,27 @@ export default Ember.Route.extend({
       });
     },
     orderBlend(params){
+      var _this = this;
       var blendQuantity = params.quantity;
       var blendName = params.blendObject.data.name;
       var blendRecipeArray = params.blendObject.data.recipe;
           this.store.query('roasted', {
             orderBy: 'name'
         }).then(function(response){
+          _this.set('roastedOrderArray', []);
           var roastArray = response.content;
           for (var i = 0; i < roastArray.length; i++) {
             for (var j = 0; j < blendRecipeArray.length; j++) {
+              if (roastArray[i]._data.name === blendRecipeArray[j].name && roastArray[i]._data.weight < (blendQuantity * blendRecipeArray[j].percent)) {
+                var newRoastWeight = (blendQuantity * blendRecipeArray[j].percent) - roastArray[i]._data.weight;
+                _this.get('roastedOrderArray').pushObject([roastArray[i]._data.name, newRoastWeight]);
+                alert("You need to roast " + newRoastWeight + " kg of " + roastArray[i]._data.name);
+              }
               if (roastArray[i]._data.name === blendRecipeArray[j].name && roastArray[i]._data.weight > (blendQuantity * blendRecipeArray[j].percent)) {
-                var newRoastWeight = roastArray[i]._data.weight - (blendQuantity * blendRecipeArray[j].percent)
-                console.log(roastArray[i])
+                alert("You have enough of " + roastArray[i]._data.name + " to complete this order. Continue to order-fulfillment form.");
               }
             }
-
           }
-
         })
       },
     }
